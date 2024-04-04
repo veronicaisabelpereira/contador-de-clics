df1d8232871b93a9d16a369169b0b2d039d534fc
veronicaisabelpereira <veronicaisabelpereira80@gmail.com>
2024-04-04 11:11:59 -0300

    Inicializar proyecto de React

---
+++ src/componentes/Contador.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../hojas-de-estilos/Contador.css";
+
+function Contador({ numClics }) {
+  return <div className="contador">{numClics}</div>;
+}
+export default Contador;
